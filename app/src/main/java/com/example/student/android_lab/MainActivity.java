4ab8c78ceba04488c475f273716735386f2b230c
Your Name <you@example.com>
2017-05-05 13:21:57 +0200

    dwa

--- app/src/main/java/com/example/student/android_lab/MainActivity.java
+++ app/src/main/java/com/example/student/android_lab/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.student.android_lab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -20,12 +24,25 @@ public class MainActivity extends AppCompatActivity {
         this.target = new ArrayList<String>();
         this.target.addAll(Arrays.asList(values));
 
-        this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
+        //this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
 
         ListView listview = (ListView) findViewById(R.id.listView);
         listview.setAdapter(this.adapter);
 
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public void nowyWpis(MenuItem mi)
+    {
+        Intent intencja = new Intent(this, DodajWpis.class);
+        startActivityForResult(intencja, 1);
     }
 
 }
